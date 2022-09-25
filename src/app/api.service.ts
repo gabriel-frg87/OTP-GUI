8c1adf6a5eda3d552b48f1ae0dd608d517bd6e3e
Gabriel <gabrielfolea1987@yahoo.com>
2022-09-25 19:16:49 +0300

    GUI Updates

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -6,5 +7,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
 
+  public otpPassword?: OtpPassword;
+  public isValid?: boolean;
+
   constructor(private httpClient: HttpClient) { }
+
+
+  public getOtp(name: string, length: number): Observable<OtpPassword> {
+    return this.httpClient.get<OtpPassword>('https://localhost:7060/api/TOTP/user/' + name + '/time/' + length);
+  }
+
+  public currentlyValid(otp: string, name: string, length: number) {
+    return this.httpClient.get<boolean>('https://localhost:7060/api/TOTP/verify/' + otp + '/' + name + '/' + length)
+  }
+
+}
+
+interface OtpPassword {
+  password: string;
+  remainingSeconds: number;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public employees?: Employee[];
+  public otpPassword?: OtpPassword;
+  public isValid: any;
 
-  constructor(http: HttpClient) {
+  constructor(http: HttpClient, private myService: ApiService) {
     http.get<Employee[]>('https://localhost:7060/api/Employee').subscribe(result => {
       this.employees = result;
     }, error => console.error(error));
   }
 
   title = 'Employees';
+
+  onClickGetOtp(name: string, length: number) {
+    this.myService.getOtp(name, length).subscribe(result => {
+      this.otpPassword = result;
+      this.isValid = null;
+    }, error => console.error(error));    
+  }
+
+  onClickVerifyOtp(otp : string, name: string, length: number) {
+    this.myService.currentlyValid(otp, name, length).subscribe(result => {
+      this.isValid = result;
+    }, error => console.error(error));
+  }
 }
 
 interface Employee {
@@ -23,4 +39,9 @@ interface Employee {
   userName: string;
   logIn: Date;
   logOut: Date;
+}
+
+interface OtpPassword {
+  password: string;
+  remainingSeconds: number;
 }
