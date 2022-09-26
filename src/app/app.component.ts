d0b349c4689296c60ecbad6e3e97d67e1233e664
Gabriel <gabrielfolea1987@yahoo.com>
2022-09-26 19:22:34 +0300

    Added length for the time window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ApiService } from './api.service';
 
 @Component({
@@ -7,10 +7,12 @@ import { ApiService } from './api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public employees?: Employee[];
   public otpPassword?: OtpPassword;
   public isValid: any;
+  @ViewChild('secondsInput') length!: ElementRef;
 
   constructor(http: HttpClient, private myService: ApiService) {
     http.get<Employee[]>('https://localhost:7060/api/Employee').subscribe(result => {
@@ -20,15 +22,15 @@ export class AppComponent {
 
   title = 'Employees';
 
-  onClickGetOtp(name: string, length: number) {
-    this.myService.getOtp(name, length).subscribe(result => {
+  onClickGetOtp(name: string) {
+    this.myService.getOtp(name, this.length.nativeElement.value).subscribe(result => {
       this.otpPassword = result;
       this.isValid = null;
     }, error => console.error(error));    
   }
 
-  onClickVerifyOtp(otp : string, name: string, length: number) {
-    this.myService.currentlyValid(otp, name, length).subscribe(result => {
+  onClickVerifyOtp(otp : string, name: string) {
+    this.myService.currentlyValid(otp, name, this.length.nativeElement.value).subscribe(result => {
       this.isValid = result;
     }, error => console.error(error));
   }
